54362d48fb41a48ddba99f79fb833221594236dc
ctaylor6914 <100639042+ctaylor6914@users.noreply.github.com>
2022-03-04 09:16:11 -0500

    Add files via upload

--- HighLow_A05/win.aspx.cs
+++ HighLow_A05/win.aspx.cs
@@ -8,25 +8,24 @@ using System.Web.UI.WebControls;
  * Programmer   : Colby Taylor 8466914
  * Course       : PROG2001 Web Design and Development 
  * Assignment   : A05-ASP.NET High Low Game
- * File         : Default.aspx.cx
+ * File         : win.aspx.cx
  * Date         : 11/15/2021
- * Description  : page 1/4 This page gathers the users name and 
- *              : creates a session variable for use in the other
- *              : pages of the web app then directs the getRange.aspx
+ * Description  : page 4/4 This page displays that the user has won
+ *              : the game and displays a button "Play Again?"
+ *              : if play again is pressed the page is changed to 2/4 
+ *              : to gather the users desired range. 
  */
-
 namespace HighLow_A05
 {
-    public partial class Default : System.Web.UI.Page
+    public partial class win : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+
         }
 
-        protected void btnEnter_Click(object sender, EventArgs e)
+        protected void btnPlayAgain_Click(object sender, EventArgs e)
         {
-            Session["userName"] = name.Text;
             Response.Redirect("~/getRange.aspx");
         }
     }
